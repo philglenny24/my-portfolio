046609a5b68dc24f463a4089412b39a61396c11d
philglenny24 <glennytronephilip@gmail.com>
2026-03-25 20:29:37 +0300

    add about page

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function scrollToSection() {
-    document.getElementById("project").scrollIntoView({
-        behaviour: "smooth"
+function toggleMenu() {
+    const nav = document.getElementById("nav");
+    nav.classList.toggle("active");
+}
+
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
     });
 }
