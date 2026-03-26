f0f52cf7370b1398da0bba2c21093b32375d0863
philglenny24 <glennytronephilip@gmail.com>
2026-03-26 08:59:02 +0300

    remove script.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const nav = document.getElementById("nav");
-    nav.classList.toggle("active");
-}
-
-function scrollToSection(id) {
-    document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
-}
